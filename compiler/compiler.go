ebbf6bd7be24db95227b5e478ef25dd8c90e42dc
fanyingfx <fanyingfx@outlook.com>
2024-11-22 08:44:04 +0800

    reformat

--- compiler/compiler.go
+++ compiler/compiler.go
@@ -6,22 +6,24 @@ import (
 	"monkey/code"
 	"monkey/object"
 )
-type Compiler struct{
+
+type Compiler struct {
 	instructions code.Instructions
-	constants []object.Object
+	constants    []object.Object
 }
-func New() *Compiler{
+
+func New() *Compiler {
 	return &Compiler{
 		instructions: code.Instructions{},
-		constants: []object.Object{},
+		constants:    []object.Object{},
 	}
 }
-func (c *Compiler)Compile(node ast.Node) error{
-	switch node := node.(type){
+func (c *Compiler) Compile(node ast.Node) error {
+	switch node := node.(type) {
 	case *ast.Program:
-		for _,s := range node.Statements{
-			err:= c.Compile(s)
-			if err != nil{
+		for _, s := range node.Statements {
+			err := c.Compile(s)
+			if err != nil {
 				return err
 			}
 		}
@@ -33,48 +35,47 @@ func (c *Compiler)Compile(node ast.Node) error{
 	case *ast.InfixExpression:
 		err := c.Compile(node.Left)
 		if err != nil {
-			return  err
+			return err
 		}
 		err = c.Compile(node.Right)
 		if err != nil {
-			return  err
+			return err
 		}
-		switch node.Operator{
+		switch node.Operator {
 		case "+":
 			c.emit(code.OpAdd)
 		default:
-			return fmt.Errorf("unknown operator %s",node.Operator)
+			return fmt.Errorf("unknown operator %s", node.Operator)
 		}
 	case *ast.IntegerLiteral:
 		integer := &object.Integer{Value: node.Value}
-		c.emit(code.OpConstant,c.addConstant(integer))
-		
+		c.emit(code.OpConstant, c.addConstant(integer))
 
-	
 	}
 	return nil
 }
-func (c *Compiler) addConstant(obj object.Object) int{
-	c.constants = append(c.constants,obj)
-	return len(c.constants)-1
+func (c *Compiler) addConstant(obj object.Object) int {
+	c.constants = append(c.constants, obj)
+	return len(c.constants) - 1
 }
-func (c *Compiler) emit(op code.Opcode,operands ...int) int{
-	ins :=code.Make(op,operands...)
+func (c *Compiler) emit(op code.Opcode, operands ...int) int {
+	ins := code.Make(op, operands...)
 	pos := c.addInstruction(ins)
 	return pos
 }
-func (c *Compiler) addInstruction(ins []byte)int{
+func (c *Compiler) addInstruction(ins []byte) int {
 	posNewInstruction := len(c.instructions)
-	c.instructions = append(c.instructions,ins...)
+	c.instructions = append(c.instructions, ins...)
 	return posNewInstruction
 }
-func(c *Compiler)Bytecode()*Bytecode{
+func (c *Compiler) Bytecode() *Bytecode {
 	return &Bytecode{
 		Instructions: c.instructions,
-		Constants: c.constants,
+		Constants:    c.constants,
 	}
 }
-type Bytecode struct{
+
+type Bytecode struct {
 	Instructions code.Instructions
-	Constants []object.Object
+	Constants    []object.Object
 }
